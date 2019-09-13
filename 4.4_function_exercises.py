ff7ba13b0fc9172e6a7232a19f3b49975c095518
Misty <ms.misty.garcia@gmail.com>
2019-09-13 13:34:01 -0500

    updated exercises

--- 4.4_function_exercises.py
+++ 4.4_function_exercises.py
@@ -75,22 +75,24 @@ get_letter_grade(55)
 get_letter_grade(87)
 
 # 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
+# 
+# def remove_vowels(string):
+#     new_list = list(string)
+#     for x in new_list:
+#         if is_vowel(x):
+#             while new_list.count(x):
+#                 new_list.remove(x)
+#     return "".join(new_list)
+
 def remove_vowels(string):
     for x in string:
         if is_vowel(x):
-            new_string = string.replace(x,"")
-    return new_string
+            string = string.replace(x,"")
+    return string 
 
-def remove_vowels(string):
-    new_list = list(string)
-    print(new_list)
-    for x in new_list:
-        if is_vowel(x):
-            new_list.remove(x)
-    return new_list.join()
-
-remove_vowels("hello")
-
+remove_vowels("helotie")
+remove_vowels("this is a test of words")
+remove_vowels("bookeeper")
 
 # 10. Define a function named normalize_name. It should accept a string and return a valid python identifier, that is:
 # anything that is not a valid python identifier should be removed
@@ -101,13 +103,80 @@ remove_vowels("hello")
 # Name will become name
 # First Name will become first_name
 # % Completed will become completed
+def normalize_name(string):
+    valid_string = string
+    for x in valid_string:
+        if not valid_string.isalnum():
+            valid_string = valid_string.replace(x,"")
+    return valid_string.strip().replace(" ", "_").lower()
+
+normalize_name("Misty")
+normalize_name("  mistYYY ")
+normalize_name("% $ Completed %  ")
 
 # 11. Write a function named cumsum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 # cumsum([1, 1, 1]) returns [1, 2, 3]
 # cumsum([1, 2, 3, 4]) returns [1, 3, 6, 10]
+def cumsum(numbers):
+    for x in range(len(numbers)):
+        if x != 0:
+            numbers[x] = numbers[x] + numbers[x-1]
+    return numbers
+
+cumsum([1,1,1])
+cumsum([1,2,3,4])
 
 # B1. Create a function named twelveto24. It should accept a string in the format 10:45am or 4:30pm and return a string that is the representation of the time in a 24-hour format. Bonus write a function that does the opposite.
+def twelveto24(string):
+    if string[-2:len(string)] == "am":
+        return string[0:len(string)-2]
+    else:
+        if len(string) == 6:
+            string = "0" + string
+        string = string[0:len(string)-2]
+        hour = int(string[0:2]) + 12
+        string = str(hour) + string[-3:len(string)]
+        return string
+
+twelveto24("3:33am")
+twelveto24("10:45am")
+twelveto24("4:30pm")
+twelveto24("11:15pm")
+
+def twentyfourto12(string):
+    if len(string) == 4:
+        string = "0" + string
+    if int(string[0:2]) <= 12:
+        return string[1:len(string)] + "am"
+    else:
+        hour = int(string[0:2]) - 12
+        return str(hour) + string[2:len(string)] + "pm"
+
+twentyfourto12("3:33")
+twentyfourto12("23:15")
+twentyfourto12("16:45")
+
 # B2. Create a function named col_index. It should accept a spreadsheet column name, and return the index number of the column.
 # col_index('A') returns 1
 # col_index('B') returns 2
 # col_index('AA') returns 27
+ def col_index(colum_name):
+    if len(colum_name) == 1:
+        return ord(colum_name.upper()) - 64
+    index = 0
+    count = 0
+    for x in colum_name:
+        index =  count * 26 + ord(x.upper()) - 64
+        count += 1
+    return index
+
+col_index("A")
+col_index("a")
+col_index("z")
+col_index("AA")
+col_index("AB")
+col_index("AZ")
+
+col_index("BA")
+col_index("ZZ")
+col_index("AAA")
