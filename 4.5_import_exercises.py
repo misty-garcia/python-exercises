6cbe137db229b398c33ddcbc05893d2b5a1492af
Misty <ms.misty.garcia@gmail.com>
2019-09-16 15:14:09 -0500

    updated imports

--- 4.5_import_exercises.py
+++ 4.5_import_exercises.py
@@ -16,18 +16,21 @@ is2("2")
 import itertools as it
 
 len(list(it.permutations('abcd123')))
+list(it.combinations("abcd","123"))
 
 # How many different ways can you combine two of the letters from "abcd"?
 len(list(it.permutations('abcd',2)))
+list(it.combinations('abcd',2))
+len(list(it.combinations('abcd',2)))
+
 
 # Save this file as profiles.json inside of your exercises directory. Use the load function from the json module to open this file, it will produce a list of dictionaries. Using this data, write some code that calculates and outputs the following information:
-
 import json
 
 with open('profiles.json', 'r') as f:
     new_dict = json.load(f)
 
-# load profiles.json
+profiles = new_dict
 
 # Total number of users
 len(new_dict)
@@ -37,7 +40,6 @@ active_list = []
 for x in new_dict:
     if x["isActive"]:
         active_list.append(x["name"])
-        print(active_list)
 len(active_list)
 
 # Number of inactive users
@@ -60,28 +62,43 @@ round(total,2)
 round(total / len(new_dict),2)
 
 # User with the lowest balance
-balance_list = []
-for x in new_dict:
-    balance_list.append(x["balance"])
-min(balance_list)
+def handle_balance(s):
+    return float(s[1:].replace(',', ''))
+
+user_with_the_lowest_balance = new_dict[0]
+for user in profiles[1:]:
+    if handle_balance(user['balance']) < handle_balance(user_with_the_lowest_balance['balance']):
+        user_with_the_lowest_balance = user
+user_with_the_lowest_balance["name"]
+user_with_the_lowest_balance["balance"]
+
+min(profiles, key=lambda profile: handle_balance(profile["balance"]))
 
 # User with the highest balance
-balance_list = []
-for x in new_dict:
-    balance_list.append(x["balance"])
-max(balance_list)
+user_with_the_highest_balance = profiles[0]
+for user in profiles[1:]:
+    if handle_balance(user['balance']) > handle_balance(user_with_the_highest_balance['balance']):
+        user_with_the_highest_balance = user
+user_with_the_highest_balance["name"]
+user_with_the_highest_balance["balance"]
+
+max(profiles, key=lambda profile: handle_balance(profile["balance"]))
 
 # Most common favorite fruit
-fruit_list = []
-for x in new_dict:
-    fruit_list.append(x["favoriteFruit"])
-max(fruit_list)
+from collections import Counter
+max(Counter([p['favoriteFruit'] for p in profiles]))
+
+fruit_count = {}
+for profile in profiles:
+    fruit = profile['favoriteFruit']
+    if fruit in fruit_count:
+        fruit_count[fruit] += 1
+    else:
+        fruit_count[fruit] = 1
+fruit_count
 
 # Least most common favorite fruit
-fruit_list = []
-for x in new_dict:
-    fruit_list.append(x["favoriteFruit"])
-min(fruit_list)
+min(Counter([p['favoriteFruit'] for p in profiles]))
 
 # Total number of unread messages for all users
 total = 0
@@ -90,4 +107,11 @@ for x in new_dict:
     num_index_end = x["greeting"].find("unread")
     num = int(x["greeting"][num_index_begin + 5:num_index_end])
     total += num
-print(total)
+total
+
+def extract_digits(s):
+    return int("".join([c for c in s if c.isdigit()]))
+
+greetings = [profile['greeting'] for profile in profiles]
+sum([extract_digits(greeting) for greeting in greetings])
+
