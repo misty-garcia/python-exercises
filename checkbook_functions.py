8170a47948ebade9195ed001ed2d6cfb887d98f1
Misty <ms.misty.garcia@gmail.com>
2019-09-17 16:59:01 -0500

    updated checkbook with extras

---
+++ checkbook_functions.py
@@ -0,0 +1,5 @@
+def records_returned(numb):
+    if numb > 0:
+        print(f"Records returned: {numb}")
+    else:
+        print("There are no records matching selected criteria.")
--- checkbook_terminal_extra.py
+++ checkbook_terminal_extra.py
@@ -1,12 +1,11 @@
 import json
 from datetime import datetime
+import time
+import checkbook_functions as cf
 data = []
 
 print("")
 print("~~ Welcome to your checkbook~~") #prints opening statement, does not repeat
-
-with open("transactions.txt") as f:
-    data = json.load(f) #open latest data list as a string for transactions
 
 while True: #repeats until the user exits
     print("""
@@ -18,31 +17,43 @@ These are your options via the terminal:
     5) exit
     """) #shows user options every time
 
+    with open("transactions.txt") as f:
+        data = json.load(f) #open latest data list
+
+    balance = 0 #calculate current balance every time
+    for x in data:
+        if x["type"] == "deposit":
+            balance = balance + float(x["amount"].strip("$"))
+        elif x["type"] == "withdraw":
+            balance = balance - float(x["amount"].strip("$"))
+
     while True: #repeats until user enters a valid entry
+        time.sleep(1)
         user_select = input("What would you like to do today? ")
         if user_select in "12345": #only valid entries
             break
         else:
             print("Invalid entry.")
  
-  
     if user_select == "1":
-        balance = (open("balance.txt","r")) 
-        print("")
-        print(f"Your balance is ${balance.readline()}.")  #reads current balance
+        print(f"Your balance is ${balance}.")
 
     elif user_select == "2":
-        balance = (open("balance.txt","r"))
-        change_balance = input("How much would you like to withdraw? ")
+        while True: #repeats until numeric entry
+            change_balance = input("How much would you like to withdraw? ")
+            if change_balance.replace(".","").isdigit():
+                break
+            else:
+                print("This is not a numeric value.")
         change_balance = format(float(change_balance), ".2f")
-        change_description = input("Enter a description (optional): ")
-        # if change_description = nulltype
+        print(f"Withdraw amount: ${change_balance}")
+        change_description = input("Description (optional): ")
 
-        after_change = format((float(balance.read()) - float(change_balance)), ".2f")
-        print(f"Your balance is ${after_change}.") #shows new balance
-    
-        balance = (open("balance.txt", "w"))
-        balance.write(after_change) #writes new balance to balance sheet
+        after_change = format((float(balance) - float(change_balance)), ".2f")
+        print("")
+        print(f"Your new balance is ${after_change}.") #shows new balance
+        if float(after_change) < 0:
+            print("***Warning: Negative balance. Will incur overdraft fees.***")
 
         data.append(
             {
@@ -56,18 +67,20 @@ These are your options via the terminal:
         with open("transactions.txt", "w") as f:
             json.dump(data,f) #dump data list into file
         
-    
     elif user_select == "3":
-        balance = (open("balance.txt","r"))
-        change_balance = input("How much would you like to deposit? ")
+        while True: #repeats enter numeric entry
+            change_balance = input("How much would you like to deposit? ")
+            if change_balance.replace(".","").isdigit():
+                break
+            else:
+                print("This is not a numeric value.")
         change_balance = format(float(change_balance), ".2f")
-        change_description = input("Enter a description (optional): ")
+        print(f"Deposit amount: ${change_balance}")
+        change_description = input("Description (optional): ")
 
-        after_change = format((float(balance.read()) + float(change_balance)), ".2f")
-        print(f"Your balance is ${after_change}.") #shows new balance
-    
-        balance = (open("balance.txt", "w"))
-        balance.write(after_change) #writes new balance to balance sheet
+        after_change = format((float(balance) + float(change_balance)), ".2f")
+        print("")
+        print(f"Your new balance is ${after_change}.") #shows new balance
 
         data.append(
             {
@@ -82,40 +95,80 @@ These are your options via the terminal:
             json.dump(data, f) #dump data list into file
             
     elif user_select == "4":
+        print("")
         print("Available transactions:")
-        with open("transactions.txt") as f:
-            data = json.load(f) #load current transactions
 
         while True:
             print("    a) all transactions")
             print("    b) withdraws")
             print("    c) deposits")
+            print("    d) search by date")
+            print("    e) search by key word")
+            print("")
             user_select_type = input("Which would you like to view? ")
-            if user_select_type in "abc": #verifies valid selection
+            if user_select_type in "abcde": #verifies valid selection
                 break
             else:
                 print("Invalid entry.")
         if user_select_type == "a":
             print("")
             print("Past transactions:")
+            available_transaction = 0
             for x in data:
-                print(x) #prints all transactions   
+                print(x) #prints all transactions
+                available_transaction += 1
+            cf.records_returned(available_transaction)
         elif user_select_type == "b":
             print("")
             print("Past withdraws:")
+            available_transaction = 0
             for x in data:
                 if x["type"] == "withdraw":
-                    print(x) #prints all withdraws   
+                    print(x) #prints all withdraws
+                    available_transaction += 1
+            cf.records_returned(available_transaction)  
         elif user_select_type == "c":
             print("")
             print("Past deposits:")
+            available_transaction = 0
             for x in data:
                 if x["type"] == "deposit":
-                    print(x) #prints all deposits 
-    else: 
+                    print(x) #prints all deposits
+                    available_transaction += 1
+            cf.records_returned(available_transaction) 
+        elif user_select_type == "d":
+            print("")
+            while True:
+                select_date = input("Enter desired date (mm/dd/yy): ")
+                if select_date[2] != '/':
+                    print("Invalid entry.")
+                    continue
+                elif select_date[5] != '/':
+                    print("Invalid entry.")
+                else:
+                    break
+            print("")
+            print(f"Transactions on {select_date}:")
+            available_transaction = 0
+            for x in data:
+                if x["date"] == select_date:
+                    print(x)
+                    available_transaction += 1
+            cf.records_returned(available_transaction)
+        else: 
+            print("")
+            select_key = input("Enter desired keyword: ")
+            print("")
+            print(f"Transactions with '{select_key}':")
+            available_transaction = 0
+            for x in data:
+                if x["description"].find(select_key) >= 0:
+                    print(x)
+                    available_transaction += 1
+            cf.records_returned(available_transaction)
+    else:
         print("")
-        print("Thanks! Have a great day!")
-        print("")
-        balance.close() 
+        print("Thanks! Have a great day!") 
+        print("")  
         break #exits the program
 
